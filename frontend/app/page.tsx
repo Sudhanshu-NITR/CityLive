b2c156d8e0615f5b44909e2bee446fc00ceec87a
Sudhanshu-NITR <sudhanshu.kadam.99@gmail.com>
2026-04-22 19:54:46 +0530

    Initialize the frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-white text-3xl">Hi</h1>
+    </div>
+  );
+}
